ebfb40e4c9b18ae9ca86f9084b82f826b24db4e2
Rob <rob@robsheldon.com>
2022-12-28 12:09:30 -0800

    debian/openbox/firefox: automatically attach Downloads shared folder, run Firefox from an unprivileged user account

--- debian/bullseye/openbox/firefox/setup.sh
+++ debian/bullseye/openbox/firefox/setup.sh
@@ -1,5 +1,20 @@
+echo "Creating firefox user"
+useradd -g vboxsf -m -s /bin/bash firefox
+cp -r /home/vagrant/.config /home/firefox/
+# Automount the downloads shared folder, if available.
+# I wish there were a better way to do this, but I can't find any way
+# to poll the guest environment for available shared folders. Instead,
+# we try to manually mount the shared folder, and if that works, then
+# update /etc/fstab to automatically handle it on reboot.
+mkdir -p /home/firefox/Downloads
+if mount -t vboxsf downloads /home/firefox/Downloads 2>/dev/null; then
+    echo "Attaching Downloads shared folder"
+    echo "downloads              /home/firefox/Downloads         vboxsf  defaults,uid=firefox,gid=vboxsf         0       0" | tee -a /etc/fstab >/dev/null
+fi
+
 echo "Installing firefox-esr"
 apt -qy install firefox-esr >/dev/null
 
 echo "Configuring autostart"
-echo '/usr/bin/firefox &' | tee -a /home/vagrant/.config/openbox/autostart
+sed -i -e 's/^autologin-user=.*$/autologin-user=firefox/g' /etc/lightdm/lightdm.conf.d/01_autologin.conf
+echo '/usr/bin/firefox &' | tee -a /home/firefox/.config/openbox/autostart
