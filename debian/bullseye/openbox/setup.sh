5664f8f6e2faa1dc582cd1c3852e3ff9bbf3e191
Rob <rob@robsheldon.com>
2023-01-03 13:29:43 -0800

    Add "development" VM (alacritty is not working, may need to be removed); assorted setup.sh fixes throughout the tree

--- debian/bullseye/openbox/setup.sh
+++ debian/bullseye/openbox/setup.sh
@@ -1,8 +1,3 @@
-# Mute annoying Perl locale warnings on each-and-every-dang apt invocation.
-locale-gen en_US.UTF-8
-localedef -i en_US -f UTF-8 en_US.UTF-8
-export LANGUAGE=en_US.UTF-8 LANG=en_US.UTF-8 LC_ALL=en_US.UTF-8
-
 # Turn off syslog; for this use, it isn't needed, and just adds unnecessary disk i/o.
 systemctl disable rsyslog
 
@@ -13,37 +8,15 @@ Storage=none
 ForwardToSyslog=yes
 JOURNALD
 systemctl daemon-reload
-
-# Clean out some preinstalled cruft before upgrading.
-# These need to all be in one command, otherwise initramfs gets rebuilt repeatedly.
-echo "Cleaning up preinstalled cruft"
-apt -qy purge gcc-9-base install-info ppp nftables os-prober reportbug rsyslog tasksel telnet >/dev/null
-
-# "fasttrack" is required for installing virtualbox-guest-x11
-echo "Adding virtualbox-guest repository"
-cat <<'FASTTRACK' | tee /etc/apt/sources.list.d/fasttrack.list >/dev/null
-deb https://fasttrack.debian.net/debian-fasttrack/ bullseye-fasttrack main contrib
-deb https://fasttrack.debian.net/debian-fasttrack/ bullseye-backports-staging main contrib
-FASTTRACK
-apt -qy update >/dev/null
-apt -qy install fasttrack-archive-keyring >/dev/null
-apt -qy update >/dev/null
-
-# Make sure everything is up to date.
-echo "Updating preinstalled packages"
-apt -qy upgrade >/dev/null
-
-# swapspace prevents OOM death on VMs
-echo "Installing utilities"
-apt -qy install aptitude swapspace >/dev/null
+apt-get -qy purge rsyslog
 
 echo "Installing xorg"
-apt -qy --no-install-recommends install xorg >/dev/null
+apt-get -qy --no-install-recommends install xorg >/dev/null
 
 # lightdm is required for basic desktop application support, and requires additional
 # recommended packages to work properly.
 echo "Installing desktop environment"
-apt -qy install lightdm openbox virtualbox-guest-x11 >/dev/null
+apt-get -qy install lightdm openbox virtualbox-guest-x11 >/dev/null
 
 echo "Configuring desktop environment"
 mkdir -p /home/vagrant/.config/openbox
@@ -83,7 +56,7 @@ AUTOLOGIN
 
 # Post-install cleanup
 echo "Cleaning up"
-apt -qy purge doc-debian debian-faq genisoimage man-db manpages plymouth plymouth-label qemu-utils xorg-docs-core >/dev/null
-apt -qy --purge autoremove >/dev/null
+apt-get -qy purge doc-debian debian-faq genisoimage man-db manpages plymouth plymouth-label qemu-utils xorg-docs-core >/dev/null
+apt-get -qy --purge autoremove >/dev/null
 dpkg -l | grep '^rc' | awk '{print $2}' | xargs apt -qy purge
-apt -qy clean >/dev/null
+apt-get -qy clean >/dev/null
---
+++ debian/bullseye/setup.sh
@@ -0,0 +1,29 @@
+export DEBIAN_FRONTEND=noninteractive
+
+# Clean out some preinstalled cruft before upgrading.
+# These need to all be in one command, otherwise initramfs gets rebuilt repeatedly.
+echo "Cleaning up preinstalled cruft"
+apt-get -qy purge gcc-9-base install-info ppp nftables os-prober reportbug tasksel telnet >/dev/null
+
+# "fasttrack" is required for installing virtualbox-guest-x11
+echo "Adding virtualbox-guest repository"
+cat <<'FASTTRACK' | tee /etc/apt/sources.list.d/fasttrack.list >/dev/null
+deb https://fasttrack.debian.net/debian-fasttrack/ bullseye-fasttrack main contrib
+deb https://fasttrack.debian.net/debian-fasttrack/ bullseye-backports-staging main contrib
+FASTTRACK
+apt-get -qy update >/dev/null
+apt-get -qy install fasttrack-archive-keyring >/dev/null
+apt-get -qy update >/dev/null
+
+# Make sure everything is up to date.
+echo "Updating preinstalled packages"
+apt-get -qy upgrade >/dev/null
+
+# swapspace prevents OOM death on VMs
+echo "Installing utilities"
+apt-get -qy install apt-transport-https aptitude swapspace language-pack-en >/dev/null
+
+# Mute annoying Perl locale warnings on each-and-every-dang apt invocation.
+locale-gen en_US.UTF-8
+localedef -i en_US -f UTF-8 en_US.UTF-8
+export LANGUAGE=en_US.UTF-8 LANG=en_US.UTF-8 LC_ALL=C
