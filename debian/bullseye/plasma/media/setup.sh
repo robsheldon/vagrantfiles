031cdcea3004d0762dc6336749b0335de6da7185
Rob <rob@robsheldon.com>
2023-12-17 15:29:57 -0800

    Configure plasma/media guest to match user account to host:
    * Also update vram for full-screen media usage on larger display
    * Move the Downloads folder automount into the new user account setup block in the Vagrantfile
    * Clean up the setup.sh file for this guest a bit

--- debian/bullseye/plasma/media/setup.sh
+++ debian/bullseye/plasma/media/setup.sh
@@ -1,31 +1,10 @@
 export DEBIAN_FRONTEND=noninteractive
 
-echo "Creating dev user"
-useradd -U -G vboxsf,sudo -m -s /bin/bash developer
-cp -r /home/vagrant/.config /home/developer/
-chown -R developer:developer /home/developer/.config
-
-# Automount the downloads shared folder, if available.
-# I wish there were a better way to do this, but I can't find any way
-# to poll the guest environment for available shared folders. Instead,
-# we try to manually mount the shared folder, and if that works, then
-# update /etc/fstab to automatically handle it on reboot.
-mkdir -p /home/developer/Downloads
-if mount -t vboxsf downloads /home/developer/Downloads 2>/dev/null; then
-    echo "Attaching Downloads shared folder"
-    echo "downloads              /home/developer/Downloads         vboxsf  defaults,uid=developer,gid=vboxsf         0       0" | tee -a /etc/fstab >/dev/null
-fi
-
 echo "Installing curl, git, gzip, rsync, wget"
 apt-get -qy install curl git gzip rsync wget >/dev/null
 
 echo "Installing Plasma applications"
-apt-get -qy install konsole dolphin >/dev/null
-
-echo "Installing sublime-text"
-wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | gpg --dearmor | tee /etc/apt/trusted.gpg.d/sublimehq-archive.gpg >/dev/null
-echo "deb https://download.sublimetext.com/ apt/stable/" | tee /etc/apt/sources.list.d/sublime-text.list
-apt-get -qy update >/dev/null && apt-get -qy install sublime-text >/dev/null
+apt-get -qy install dolphin >/dev/null
 
 echo "Installing firefox-esr"
 apt-get -qy install firefox-esr >/dev/null
@@ -33,8 +12,11 @@ apt-get -qy install firefox-esr >/dev/null
 echo "Installing yakuake"
 apt-get -qy install yakuake >/dev/null
 
+echo "Installing VLC"
+apt-get -qy install vlc >/dev/null
+
 echo "Configuring autostart"
-sed -i -e 's/^autologin-user=.*$/autologin-user=developer/g' /etc/lightdm/lightdm.conf.d/01_autologin.conf
+sed -i -e 's/^autologin-user=.*$/autologin-user=rob/g' /etc/lightdm/lightdm.conf.d/01_autologin.conf
 
 echo "Cleaning up"
 balooctl disable
